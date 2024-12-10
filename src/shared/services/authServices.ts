396001cf4aac66c3de01ea26ce7c62d1b53a7ca1
OdilDev10 <odildmartinezcuello@gmail.com>
2024-12-10 14:22:26 -0400

    Ajustes a auth login

--- src/shared/services/authServices.ts
+++ src/shared/services/authServices.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   LoginInterface,
   RegisterInterface,
@@ -17,12 +18,25 @@ export const postRegisterUser = async (data: RegisterInterface) => {
 
 export const postLoginUser = async (data: LoginInterface) => {
   try {
-    console.log(data, "data");
     const results = await instanceAxios.post("login", data);
-    console.log(results, "login");
-    // return results.data;
+    return results.data;
   } catch (error) {
-    console.log(error);
-    return "Error getting all users";
+    if (axios.isAxiosError(error)) {
+      // Error específico de Axios
+
+      if (Array.isArray(error?.response?.data?.errors)) {
+        console.log(error.response?.data?.errors, "Array");
+      }
+
+      if (error?.response?.data?.error) {
+        console.log(error.response?.data.error, "string");
+      }
+
+      return error.response?.data || "Error en el servidor";
+    } else {
+      // Otros errores no relacionados con Axios
+      console.error("Unknown Error:", error);
+      return "Error desconocido";
+    }
   }
 };
