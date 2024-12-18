446be5ade679677c964a4d562fa2e1900f1ef62e
OdilDev10 <odildmartinezcuello@gmail.com>
2024-12-18 15:58:07 -0400

    Mas cambios

--- src/modules/home/components/TaskListDashboard.tsx
+++ src/modules/home/components/TaskListDashboard.tsx
@@ -1,54 +1,9 @@
 import { Avatar, Button, List } from "antd";
 import ButtonGroup from "antd/es/button/button-group";
-import ModalCreateTask from "./ModalCreateTask";
 import { TaskInterface } from "../../../shared/interfaces/TaskInterface";
-import { useEffect, useState } from "react";
-import { getAllTasks } from "../../../shared/services/taskServices";
+import ModalCreateTask from "./ModalCreateTask";
 
 const TaskListDashboard = ({ data }: { data: TaskInterface[] }) => {
-  const [allTasks, setAllTasks] = useState<TaskInterface[]>([]);
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
-    },
-    {
-      title: "Content",
-      dataIndex: "content",
-      key: "content",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      key: "status",
-    },
-  ];
-
-  const dtoTasks = (data: any[]) => {
-    let results = data?.map((item) => {
-      return {
-        id: item?.id || "",
-        title: item?.title || "",
-        content: item?.content || "",
-        status: item?.status || "",
-      };
-    });
-    return results;
-  };
-
-  // useEffect(() => {
-  //   getAllTasks().then((data) => {
-  //     setAllTasks(dtoTasks(data.data));
-  //     console.log(data);
-  //   });
-  // }
-  // , []);
   return (
     <>
       <List
